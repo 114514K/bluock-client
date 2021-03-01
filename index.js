f93812b9218e71818e637fbcb8cd7509a46da094
loxygenk <loxygen.k@gmail.com>
2021-03-02 00:29:57 +0900

    🔧 Configure electron

---
+++ index.js
@@ -0,0 +1,29 @@
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 640,
+    height: 480
+  });
+  win.webContents.openDevTools();
+  win.loadURL(`localhost`);
+}
+
+app.on("ready", () => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
