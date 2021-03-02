15daa1a1e63a01dc3dc68eb87237e18517fe56a2
loxygenk <loxygen.k@gmail.com>
2021-03-02 13:24:00 +0900

    🔨 Setup various development scripts

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,5 @@
-import {App} from "./components/App";
+import { App } from "./components/App";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById("root")
-);
-
+ReactDOM.render(<App />, document.getElementById("root"));
